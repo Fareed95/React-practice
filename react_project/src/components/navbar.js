8d1d96fe4af80cf89a30727ed7aeb976031073d6
Fareed95 <fareedsayed95@gmail.com>
2024-09-18 17:08:54 +0530

    vikte setup

---
+++ react_project/src/components/navbar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+      <ul>
+        <li>Home</li>
+        <li>About us</li>   
+        <li>contact us</li>
+      </ul>
+    </div>
+  )
+}
+
+export default Navbar;
